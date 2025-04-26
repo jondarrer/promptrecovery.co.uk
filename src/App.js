971a7c8678f0dc90c83a738e95eb7f9e45797b6b
Vlad-SLG <silaghivlad1@gmail.com>
2025-04-26 21:07:08 +0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navigation/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
