5763ae4a926f1ced35265c3424b047251ffac898
Vlad-SLG <silaghivlad1@gmail.com>
2025-05-02 13:47:00 +0300

    mod

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navigation/Navbar";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Services from "./pages/Services/Services";
-import FAQs from "./pages/FAQs/FAQs";
-import Reviews from "./pages/Reviews/Reviews";
-import Blog from "./pages/Blog/Blog";
-import "./App.css";
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navigation/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Services from './pages/Services/Services';
+import FAQs from './pages/FAQs/FAQs';
+import Reviews from './pages/Reviews/Reviews';
+import Blog from './pages/Blog/Blog';
+import './App.css';
+
+function updateMetaTags() {
+  document.title = 'Prompt Recovery Ltd | Rapid Response, Prompt Recovery';
+
+  let metaDescription = document.querySelector('meta[name="description"]');
+  if (!metaDescription) {
+    metaDescription = document.createElement('meta');
+    metaDescription.name = 'description';
+    document.head.appendChild(metaDescription);
+  }
+  metaDescription.content =
+    'ROADSIDE RECOVERY YOU CAN RELY ON. SERVING WATFORD & SOURROUNDING AREAS. Fast, friendly and affordable help for vehicles under 4 tonnes.';
+
+  let metaKeywords = document.querySelector('meta[name="keywords"]');
+  if (!metaKeywords) {
+    metaKeywords = document.createElement('meta');
+    metaKeywords.name = 'keywords';
+    document.head.appendChild(metaKeywords);
+  }
+  metaKeywords.content = 'recovery service, roadside assistance, Watford, vehicle recovery, prompt recovery';
+}
 
 function App() {
+  React.useEffect(() => {
+    updateMetaTags();
+  }, []);
+
   return (
     <Router>
       <div className="app">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+function setupSEO() {
+  document.title = 'Prompt Recovery Ltd | Rapid Response, Prompt Recovery';
+
+  const canonicalLink = document.createElement('link');
+  canonicalLink.rel = 'canonical';
+  canonicalLink.href = 'https://promptrecovery.co.uk';
+  document.head.appendChild(canonicalLink);
+}
+
+setupSEO();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
