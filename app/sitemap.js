f4adc320a10133b62eb47c363720ac57523c07bc
Jonathan Darrer <jdarrer@findmypast.com>
2025-05-07 23:15:57 +0100

    🔍 Add a sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,46 @@
+import { MetadataRoute } from 'next';
+
+/**
+ *
+ * @returns {MetadataRoute.Sitemap}
+ */
+export default function sitemap() {
+  return [
+    {
+      url: 'https://promptrecovery.co.uk/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://promptrecovery.co.uk/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://promptrecovery.co.uk/faqs',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://promptrecovery.co.uk/reviews',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://promptrecovery.co.uk/services',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://promptrecovery.co.uk/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ];
+}
