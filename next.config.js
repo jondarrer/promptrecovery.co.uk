ab3a4610c2cc0283616a15758af0da64c78d6569
Jonathan Darrer <jdarrer@findmypast.com>
2025-05-07 21:33:03 +0100

    🧑‍💻 Add bundle analysis option

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["local.promptrecovery.co.uk"],
+  allowedDevOrigins: ['local.promptrecovery.co.uk'],
   basePath: process.env.PAGES_BASE_PATH,
   distDir: 'build',
-  output: "export",  // <=== enables static exports
+  output: 'export', // <=== enables static exports
   reactStrictMode: true,
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  experimental: {
+    optimizePackageImports: ['react-icons'], // <=== enables tree-shaking for react-icons - see https://nextjs.org/docs/app/api-reference/config/next-config-js/optimizePackageImports
+  },
 };
 
-module.exports = nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
